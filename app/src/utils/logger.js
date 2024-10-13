48cb0cdad5ec3bdf750616dff7b5c62d98ffb005
Vikitoshi <31087501+vinskasenda@users.noreply.github.com>
2024-10-13 21:15:07 +0700

    Update logger.js

--- app/src/utils/logger.js
+++ app/src/utils/logger.js
@@ -1,5 +1,5 @@
 import { createLogger, format, transports } from 'winston';
-import a7_0x2fdfdc from 'fs';
+import a7_0x4ee2c8 from 'fs';
 const {
   combine,
   timestamp,
@@ -7,16 +7,16 @@ const {
   colorize
 } = format;
 const customFormat = printf(({
-  level: _0x2f0e61,
-  message: _0x41472e,
-  timestamp: _0x4f27b1
+  level: _0x278411,
+  message: _0x84a27c,
+  timestamp: _0x4d537e
 }) => {
-  return _0x4f27b1 + " [" + _0x2f0e61 + "]: " + _0x41472e;
+  return _0x4d537e + " [" + _0x278411 + "]: " + _0x84a27c;
 });
 class Logger {
   constructor() {
     this.logger = createLogger({
-      'level': 'debug',
+      'level': "debug",
       'format': combine(timestamp({
         'format': "YYYY-MM-DD HH:mm:ss"
       }), colorize(), customFormat),
@@ -24,32 +24,32 @@ class Logger {
         'filename': "log/app.log"
       })],
       'exceptionHandlers': [new transports.File({
-        'filename': 'log/app.log'
+        'filename': "log/app.log"
       })],
       'rejectionHandlers': [new transports.File({
         'filename': "log/app.log"
       })]
     });
   }
-  ["info"](_0x550524) {
-    this.logger.info(_0x550524);
+  ['info'](_0x56f8bb) {
+    this.logger.info(_0x56f8bb);
   }
-  ["warn"](_0x25fee5) {
-    this.logger.warn(_0x25fee5);
+  ["warn"](_0x5c5446) {
+    this.logger.warn(_0x5c5446);
   }
-  ["error"](_0x4ce2d7) {
-    this.logger.error(_0x4ce2d7);
+  ["error"](_0x5b1123) {
+    this.logger.error(_0x5b1123);
   }
-  ["debug"](_0x5699b1) {
-    this.logger.debug(_0x5699b1);
+  ["debug"](_0x41586b) {
+    this.logger.debug(_0x41586b);
   }
-  ['setLevel'](_0x2f2f23) {
-    this.logger.level = _0x2f2f23;
+  ["setLevel"](_0x2af251) {
+    this.logger.level = _0x2af251;
   }
   ["clear"]() {
-    a7_0x2fdfdc.truncate("log/app.log", 0x0, _0x5ee33c => {
-      if (_0x5ee33c) {
-        this.logger.error("Failed to clear the log file: " + _0x5ee33c.message);
+    a7_0x4ee2c8.truncate("log/app.log", 0x0, _0x43f684 => {
+      if (_0x43f684) {
+        this.logger.error("Failed to clear the log file: " + _0x43f684.message);
       } else {
         this.logger.info("Log file cleared");
       }
