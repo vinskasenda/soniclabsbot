49f9b5ea9585ceab84827573ec215706057254ea
Vikitoshi <31087501+vinskasenda@users.noreply.github.com>
2024-10-14 00:27:05 +0700

    Update soniclabs.js

--- app/src/core/dapps/soniclabs.js
+++ app/src/core/dapps/soniclabs.js
@@ -1,4 +1,4 @@
 export class SONICLABS {
-  static ["URL"] = "https://airdrop.soniclabs.com/";
-  static ["PLINKOCONTRACT"] = '0xA7C492D9B3BD057845e3bC080c250ad868518478';
-}
+    static ["URL"] = "https://airdrop.soniclabs.com/";
+    static ["PLINKOCONTRACT"] = '0xA7C492D9B3BD057845e3bC080c250ad868518478';
+  }
