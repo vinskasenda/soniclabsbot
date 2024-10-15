1444b1d19f305da0c367a97dc1f4d38b47f3dd97
Vikitoshi <31087501+vinskasenda@users.noreply.github.com>
2024-10-15 21:31:16 +0700

    Update twist.js

--- app/src/utils/twist.js
+++ app/src/utils/twist.js
@@ -1,38 +1,40 @@
 import { Twisters } from 'twisters';
 import './helper.js';
-import a8_0x2b848d from './logger.js';
-import a8_0x4fdc4d from '../core/core.js';
+import a8_0x4a7f81 from './logger.js';
+import a8_0x4076a3 from '../core/core.js';
 import { privateKey } from '../../accounts/accounts.js';
 import { RPC } from '../core/network/rpc.js';
 class Twist {
   constructor() {
     this.twisters = new Twisters();
   }
-  ['log'](_0x473819 = '', _0x19fcde = '', _0x552abc = new a8_0x4fdc4d(), _0x5e0f7d) {
-    if (_0x5e0f7d == undefined) {
-      a8_0x2b848d.info("Account " + (privateKey.indexOf(_0x19fcde) + 0x1) + " - " + _0x473819);
-      _0x5e0f7d = '-';
+  ["log"](_0x2c25ae = '', _0x5d7cc6 = '', _0xa2ace7 = new a8_0x4076a3(), _0x15f230) {
+    const _0x34d3ea = privateKey.find(_0x4109a9 => _0x4109a9.pk == _0x5d7cc6);
+    const _0x3d3371 = privateKey.indexOf(_0x34d3ea);
+    if (_0x15f230 == undefined) {
+      a8_0x4a7f81.info("Account " + (_0x3d3371 + 0x1) + " - " + _0x2c25ae);
+      _0x15f230 = '-';
     }
-    const _0x256172 = _0x552abc.address ?? '-';
-    const _0x22977f = _0x552abc.balance ?? '-';
-    const _0x4efdd1 = _0x552abc.point ?? {};
-    const _0x3dce94 = _0x4efdd1.totalPoints ?? '-';
-    const _0x1fcfc9 = _0x4efdd1.today ?? '-';
-    this.twisters.put(_0x19fcde, {
-      'text': "\n================== Account " + (privateKey.indexOf(_0x19fcde) + 0x1) + " =================\nAddress      : " + _0x256172 + "\nBalance      : " + _0x22977f + " " + RPC.SYMBOL + "\nPoint        : Total " + _0x3dce94 + " | Today " + _0x1fcfc9 + "\n\nStatus : " + _0x473819 + "\nDelay : " + _0x5e0f7d + "\n=============================================="
+    const _0x4117e6 = _0xa2ace7.address ?? '-';
+    const _0x4bbd22 = _0xa2ace7.balance ?? '-';
+    const _0x31f99b = _0xa2ace7.point ?? {};
+    const _0x315b6f = _0x31f99b.totalPoints ?? '-';
+    const _0x312116 = _0x31f99b.today ?? '-';
+    this.twisters.put(_0x5d7cc6, {
+      'text': "\n================== Account " + (_0x3d3371 + 0x1) + " =================\nAddress      : " + _0x4117e6 + "\nBalance      : " + _0x4bbd22 + " " + RPC.SYMBOL + "\nPoint        : Total " + _0x315b6f + " | Today " + _0x312116 + "\n\nStatus : " + _0x2c25ae + "\nDelay : " + _0x15f230 + "\n=============================================="
     });
   }
-  ["info"](_0x25250f = '') {
+  ['info'](_0x52d71d = '') {
     this.twisters.put(0x2, {
-      'text': "\n==============================================\nInfo : " + _0x25250f + "\n=============================================="
+      'text': "\n==============================================\nInfo : " + _0x52d71d + "\n=============================================="
     });
     return;
   }
   ["clearInfo"]() {
     this.twisters.remove(0x2);
   }
-  ["clear"](_0x2cffe2) {
-    this.twisters.remove(_0x2cffe2);
+  ['clear'](_0x2d0cee) {
+    this.twisters.remove(_0x2d0cee);
   }
 }
 export default new Twist();
