f06021799a01ca95eb961b86cf23aa0013276d7a
Vikitoshi <31087501+vinskasenda@users.noreply.github.com>
2024-10-17 10:30:26 +0700

    Update logger.js

--- app/src/utils/logger.js
+++ app/src/utils/logger.js
@@ -1,5 +1,5 @@
 import { createLogger, format, transports } from 'winston';
-import a7_0x2eddf2 from 'fs';
+import a8_0x526e9b from 'fs';
 const {
   combine,
   timestamp,
@@ -7,11 +7,11 @@ const {
   colorize
 } = format;
 const customFormat = printf(({
-  level: _0x116e70,
-  message: _0x42afd2,
-  timestamp: _0x370b43
+  level: _0x5390d2,
+  message: _0x45b540,
+  timestamp: _0x33f2d1
 }) => {
-  return _0x370b43 + " [" + _0x116e70 + "]: " + _0x42afd2;
+  return _0x33f2d1 + " [" + _0x5390d2 + "]: " + _0x45b540;
 });
 class Logger {
   constructor() {
@@ -27,29 +27,29 @@ class Logger {
         'filename': "log/app.log"
       })],
       'rejectionHandlers': [new transports.File({
-        'filename': "log/app.log"
+        'filename': 'log/app.log'
       })]
     });
   }
-  ["info"](_0x180620) {
-    this.logger.info(_0x180620);
+  ["info"](_0x15e80d) {
+    this.logger.info(_0x15e80d);
   }
-  ['warn'](_0x23a3be) {
-    this.logger.warn(_0x23a3be);
+  ["warn"](_0x2f3152) {
+    this.logger.warn(_0x2f3152);
   }
-  ["error"](_0x6bcd6d) {
-    this.logger.error(_0x6bcd6d);
+  ["error"](_0x23269b) {
+    this.logger.error(_0x23269b);
   }
-  ["debug"](_0x363660) {
-    this.logger.debug(_0x363660);
+  ['debug'](_0xf21483) {
+    this.logger.debug(_0xf21483);
   }
-  ["setLevel"](_0x470a0c) {
-    this.logger.level = _0x470a0c;
+  ['setLevel'](_0x2f17a6) {
+    this.logger.level = _0x2f17a6;
   }
   ["clear"]() {
-    a7_0x2eddf2.truncate("log/app.log", 0x0, _0x165984 => {
-      if (_0x165984) {
-        this.logger.error("Failed to clear the log file: " + _0x165984.message);
+    a8_0x526e9b.truncate("log/app.log", 0x0, _0x69dac8 => {
+      if (_0x69dac8) {
+        this.logger.error("Failed to clear the log file: " + _0x69dac8.message);
       } else {
         this.logger.info("Log file cleared");
       }
