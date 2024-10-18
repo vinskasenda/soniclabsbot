afbb631bd2d60604b5244ce5d4a38b310926fa5c
Vikitoshi <31087501+vinskasenda@users.noreply.github.com>
2024-10-18 21:48:39 +0700

    Update api.js

--- app/src/api/api.js
+++ app/src/api/api.js
@@ -1,121 +1,121 @@
 import { Helper } from '../utils/helper.js';
-import a1_0x5496cd from '../utils/logger.js';
-import a1_0x16b200 from 'node-fetch';
+import a1_0x39153e from '../utils/logger.js';
+import a1_0x3f4678 from 'node-fetch';
 import { HttpsProxyAgent } from 'https-proxy-agent';
 export class API {
-  constructor(_0xf52d76, _0x59012c, _0x7a8536, _0x28aab9, _0x21ddbc) {
-    this.url = _0xf52d76;
-    this.host = _0x7a8536;
-    this.origin = _0x28aab9;
+  constructor(_0x5edfd0, _0x1f4409, _0xc472a7, _0x2802b7, _0x2670c5) {
+    this.url = _0x5edfd0;
+    this.host = _0xc472a7;
+    this.origin = _0x2802b7;
     this.ua = Helper.randomUserAgent();
-    this.something = _0x21ddbc;
-    this.proxy = _0x59012c;
+    this.something = _0x2670c5;
+    this.proxy = _0x1f4409;
   }
-  ["generateHeaders"](_0x38e4bc) {
-    const _0x17ec63 = {
-      'Accept': "application/json",
-      'Accept-Language': 'en-US,en;q=0.9,id;q=0.8',
-      'Content-Type': 'application/json',
+  ["generateHeaders"](_0x575c10) {
+    const _0x5c877e = {
+      'Accept': 'application/json',
+      'Accept-Language': "en-US,en;q=0.9,id;q=0.8",
+      'Content-Type': "application/json",
       'Sec-Fetch-Dest': 'empty',
       'Sec-Fetch-Site': "same-site",
-      'Sec-Fetch-Mode': 'cors',
+      'Sec-Fetch-Mode': "cors",
       'Cache-Control': "no-cache",
       'Connection': "keep-alive",
-      'Pragma': "no-cache"
+      'Pragma': 'no-cache'
     };
-    if (_0x38e4bc) {
-      _0x17ec63.Authorization = "Bearer " + _0x38e4bc;
+    if (_0x575c10) {
+      _0x5c877e.Authorization = "Bearer " + _0x575c10;
     }
-    return _0x17ec63;
+    return _0x5c877e;
   }
-  ["replaceSensitiveData"](_0x39cf23) {
-    if (typeof this.something === 'string') {
-      const _0x376338 = new RegExp(this.something, 'g');
-      return _0x39cf23.replace(_0x376338, '?????');
+  ["replaceSensitiveData"](_0x2f9368) {
+    if (typeof this.something === "string") {
+      const _0x5094a3 = new RegExp(this.something, 'g');
+      return _0x2f9368.replace(_0x5094a3, "?????");
     } else if (Array.isArray(this.something)) {
-      this.something.forEach(_0x4b0c4d => {
-        const _0xa98c64 = new RegExp(_0x4b0c4d, 'g');
-        _0x39cf23 = _0x39cf23.replace(_0xa98c64, "?????");
+      this.something.forEach(_0x131b43 => {
+        const _0x43eb4f = new RegExp(_0x131b43, 'g');
+        _0x2f9368 = _0x2f9368.replace(_0x43eb4f, "?????");
       });
     }
-    return _0x39cf23;
+    return _0x2f9368;
   }
-  async ["fetch"](_0x31987f, _0x2f279f, _0x2ba091, _0x38eb72 = {}, _0x37d40c = {}, _0x8c43bf, _0x1bd771 = false) {
-    const _0x569932 = _0x1bd771 ? _0x31987f : '' + this.url + _0x31987f;
+  async ['fetch'](_0x34cb06, _0x374955, _0x568fd8, _0x27b770 = {}, _0x2355ca = {}, _0x404bcc, _0x205d07 = false) {
+    const _0x5aba9f = _0x205d07 ? _0x34cb06 : '' + this.url + _0x34cb06;
     try {
-      const _0x16c1f7 = {
-        ...this.generateHeaders(_0x2ba091),
-        ..._0x37d40c
+      const _0x2f767b = {
+        ...this.generateHeaders(_0x568fd8),
+        ..._0x2355ca
       };
-      const _0x4ce3fb = {
-        'headers': _0x16c1f7,
-        'method': _0x2f279f,
-        'referer': _0x8c43bf,
-        'referrerPolicy': "strict-origin-when-cross-origin",
-        'mode': "cors",
-        'credentials': "omit"
+      const _0x528bc4 = {
+        'headers': _0x2f767b,
+        'method': _0x374955,
+        'referer': _0x404bcc,
+        'referrerPolicy': 'strict-origin-when-cross-origin',
+        'mode': 'cors',
+        'credentials': 'omit'
       };
-      a1_0x5496cd.info(_0x2f279f + " : " + this.replaceSensitiveData(_0x569932) + " " + (this.proxy ? this.proxy : ''));
-      for (let _0xb59211 in _0x16c1f7) {
-        _0x16c1f7[_0xb59211] = this.replaceSensitiveData(_0x16c1f7[_0xb59211]);
+      a1_0x39153e.info(_0x374955 + " : " + this.replaceSensitiveData(_0x5aba9f) + " " + (this.proxy ? this.proxy : ''));
+      for (let _0x55d02c in _0x2f767b) {
+        _0x2f767b[_0x55d02c] = this.replaceSensitiveData(_0x2f767b[_0x55d02c]);
       }
-      a1_0x5496cd.info("Request Header : " + JSON.stringify(_0x16c1f7));
-      if (_0x2f279f !== "GET") {
-        _0x4ce3fb.body = '' + JSON.stringify(_0x38eb72);
-        const _0x59fc36 = this.replaceSensitiveData(_0x4ce3fb.body);
-        a1_0x5496cd.info("Request Body : " + _0x59fc36);
+      a1_0x39153e.info("Request Header : " + JSON.stringify(_0x2f767b));
+      if (_0x374955 !== "GET") {
+        _0x528bc4.body = '' + JSON.stringify(_0x27b770);
+        const _0x5a10b9 = this.replaceSensitiveData(_0x528bc4.body);
+        a1_0x39153e.info("Request Body : " + _0x5a10b9);
       }
       if (this.proxy) {
-        _0x4ce3fb.agent = new HttpsProxyAgent(this.proxy, {
+        _0x528bc4.agent = new HttpsProxyAgent(this.proxy, {
           'rejectUnauthorized': false
         });
       }
-      const _0x2fdf6 = await a1_0x16b200(_0x569932, _0x4ce3fb);
-      a1_0x5496cd.info("Response : " + _0x2fdf6.status + " " + _0x2fdf6.statusText);
-      if (_0x2fdf6.ok || _0x2fdf6.status == 0x190 || _0x2fdf6.status == 0x193) {
-        const _0x1db2ba = _0x2fdf6.headers.get("content-type");
-        let _0x1ab345;
-        if (_0x1db2ba && _0x1db2ba.includes("application/json")) {
-          _0x1ab345 = await _0x2fdf6.json();
-          _0x1ab345.status = _0x2fdf6.status;
+      const _0x1c12f8 = await a1_0x3f4678(_0x5aba9f, _0x528bc4);
+      a1_0x39153e.info("Response : " + _0x1c12f8.status + " " + _0x1c12f8.statusText);
+      if (_0x1c12f8.ok || _0x1c12f8.status == 0x190 || _0x1c12f8.status == 0x193) {
+        const _0x502475 = _0x1c12f8.headers.get('content-type');
+        let _0x2835ae;
+        if (_0x502475 && _0x502475.includes('application/json')) {
+          _0x2835ae = await _0x1c12f8.json();
+          _0x2835ae.status = _0x1c12f8.status;
         } else {
-          _0x1ab345 = {
-            'status': _0x2fdf6.status,
-            'message': await _0x2fdf6.text()
+          _0x2835ae = {
+            'status': _0x1c12f8.status,
+            'message': await _0x1c12f8.text()
           };
         }
-        if (_0x2fdf6.ok) {
-          _0x1ab345.status = 0xc8;
+        if (_0x1c12f8.ok) {
+          _0x2835ae.status = 0xc8;
         }
-        let _0x5cfa07 = JSON.stringify(_0x1ab345);
-        _0x5cfa07 = this.replaceSensitiveData(_0x5cfa07);
-        if (_0x5cfa07.length > 0xc8) {
-          _0x5cfa07 = _0x5cfa07.substring(0x0, 0xc8) + "...";
+        let _0x59bbdd = JSON.stringify(_0x2835ae);
+        _0x59bbdd = this.replaceSensitiveData(_0x59bbdd);
+        if (_0x59bbdd.length > 0xc8) {
+          _0x59bbdd = _0x59bbdd.substring(0x0, 0xc8) + '...';
         }
-        a1_0x5496cd.info("Response Data : " + _0x5cfa07);
-        return _0x1ab345;
+        a1_0x39153e.info("Response Data : " + _0x59bbdd);
+        return _0x2835ae;
       } else {
-        throw _0x2fdf6;
+        throw _0x1c12f8;
       }
-    } catch (_0x1ed861) {
-      if (_0x1ed861.status && _0x1ed861.statusText) {
-        if (_0x569932.includes("setInvited") && _0x1ed861.status == "401") {
+    } catch (_0x34a88a) {
+      if (_0x34a88a.status && _0x34a88a.statusText) {
+        if (_0x5aba9f.includes("setInvited") && _0x34a88a.status == "401") {
           return {
             'status': 0xc8
           };
         } else {
-          if (_0x569932.includes('points-by-player') && _0x1ed861.status == "500") {
+          if (_0x5aba9f.includes("points-by-player") && _0x34a88a.status == "500") {
             return {
               'status': 0x1f4,
-              ...(await _0x1ed861.json())
+              ...(await _0x34a88a.json())
             };
           } else {
-            a1_0x5496cd.error("Error : " + _0x1ed861.message);
-            throw Error(_0x1ed861.status + " - " + _0x1ed861.statusText);
+            a1_0x39153e.error("Error : " + _0x34a88a.message);
+            throw Error(_0x34a88a.status + " - " + _0x34a88a.statusText);
           }
         }
       } else {
-        throw _0x1ed861;
+        throw _0x34a88a;
       }
     }
   }
