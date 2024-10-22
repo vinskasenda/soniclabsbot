5f3ec9fb338a398029584e0bf4f94fae5c91e603
Vikitoshi <31087501+vinskasenda@users.noreply.github.com>
2024-10-22 19:10:23 +0700

    Update bless.js

--- app/src/utils/bless.js
+++ app/src/utils/bless.js
@@ -1,44 +1,45 @@
-import a6_0x18c1df from 'blessed';
-import a6_0x231e47 from './logger.js';
-import a6_0x2028b2 from '../core/core.js';
+import a6_0x763fd4 from 'blessed';
+import a6_0x18d1b5 from './logger.js';
+import a6_0x52ee42 from '../core/core.js';
 import { privateKey } from '../../accounts/accounts.js';
 import { RPC } from '../core/network/rpc.js';
+import { Config } from '../../config/config.js';
 class Bless {
   constructor() {
-    this.screen = a6_0x18c1df.screen({
+    this.screen = a6_0x763fd4.screen({
       'smartCSR': true
     });
-    this.screen.title = "SKEL DROP HUNT";
-    this.titleBox = a6_0x18c1df.box({
+    this.screen.title = "AIRDROP INSIDER";
+    this.titleBox = a6_0x763fd4.box({
       'top': 0x0,
-      'left': "center",
+      'left': 'center',
       'width': "shrink",
       'height': 0x2,
       'tags': true,
-      'content': "{center}SONICLABS ARCADE BOT{/center}\n    By: Airdrop Insider",
+      'content': "{center}SONICLABS ARCADE BOT{/center}\n    By: AIRDROP INSIDER",
       'style': {
-        'fg': 'white',
+        'fg': "white",
         'bold': true
       }
     });
     this.screen.append(this.titleBox);
-    this.subTitle = a6_0x18c1df.box({
+    this.subTitle = a6_0x763fd4.box({
       'top': 0x1,
-      'left': "center",
+      'left': 'center',
       'width': "shrink",
       'height': 0x2,
       'tags': true,
-      'content': "By: Airdrop Insider (https://t.me/AirdropInsiderID)",
+      'content': "By: Airdrop - Insider (https://t.me/AirdropInsiderID)",
       'style': {
-        'fg': 'white',
+        'fg': "white",
         'bold': true
       }
     });
     this.screen.append(this.subTitle);
-    this.tabList = a6_0x18c1df.box({
+    this.tabList = a6_0x763fd4.box({
       'top': 0x5,
-      'left': "center",
-      'width': '100%',
+      'left': 'center',
+      'width': "100%",
       'height': 0x3,
       'tags': true,
       'style': {
@@ -46,61 +47,61 @@ class Bless {
       }
     });
     this.screen.append(this.tabList);
-    this.hintBox = a6_0x18c1df.box({
+    this.hintBox = a6_0x763fd4.box({
       'bottom': 0x0,
-      'left': 'center',
-      'width': '100%',
-      'height': 0x3,
-      'tags': true,
-      'content': "{center}Use '->'(arrow right) and '<-'(arrow left) to switch between tabs{/center}",
-      'style': {
-        'fg': 'white'
-      }
-    });
-    this.screen.append(this.hintBox);
-    this.infoBox = a6_0x18c1df.box({
-      'bottom': 0x3,
       'left': "center",
       'width': "100%",
       'height': 0x3,
       'tags': true,
+      'content': "{center}Use '->'(arrow right) and '<-'(arrow left) to switch between tabs{/center}",
+      'style': {
+        'fg': "white"
+      }
+    });
+    this.screen.append(this.hintBox);
+    this.infoBox = a6_0x763fd4.box({
+      'bottom': 0x3,
+      'left': "center",
+      'width': '100%',
+      'height': 0x3,
+      'tags': true,
       'content': '',
       'style': {
-        'fg': 'white'
+        'fg': "white"
       }
     });
     this.screen.append(this.infoBox);
     this.tabs = [];
     this.currentTabIndex = 0x0;
-    privateKey.forEach((_0x2d2f40, _0x50c922) => {
-      const _0x25a191 = this.createAccountTab("Account " + (_0x50c922 + 0x1));
-      this.tabs.push(_0x25a191);
-      this.screen.append(_0x25a191);
-      _0x25a191.hide();
+    privateKey.forEach((_0x592000, _0xf73f06) => {
+      const _0x2ba237 = this.createAccountTab("Account " + (_0xf73f06 + 0x1));
+      this.tabs.push(_0x2ba237);
+      this.screen.append(_0x2ba237);
+      _0x2ba237.hide();
     });
     if (this.tabs.length > 0x0) {
       this.tabs[0x0].show();
     }
     this.renderTabList();
-    this.screen.key(['q', "C-c"], () => {
+    this.screen.key(['q', 'C-c'], () => {
       return process.exit(0x0);
     });
-    this.screen.key(['left', "right"], (_0x36651a, _0x30b90a) => {
-      if (_0x30b90a.name === 'right') {
+    this.screen.key(["left", 'right'], (_0x3f8535, _0x4d7f98) => {
+      if (_0x4d7f98.name === "right") {
         this.switchTab((this.currentTabIndex + 0x1) % this.tabs.length);
-      } else if (_0x30b90a.name === "left") {
+      } else if (_0x4d7f98.name === "left") {
         this.switchTab((this.currentTabIndex - 0x1 + this.tabs.length) % this.tabs.length);
       }
     });
     this.screen.render();
   }
-  ["createAccountTab"](_0x2c0c35) {
-    return a6_0x18c1df.box({
-      'label': _0x2c0c35,
+  ["createAccountTab"](_0x5a3b5d) {
+    return a6_0x763fd4.box({
+      'label': _0x5a3b5d,
       'top': 0x6,
       'left': 0x0,
       'width': "100%",
-      'height': 'shrink',
+      'height': "shrink",
       'border': {
         'type': "line"
       },
@@ -114,43 +115,48 @@ class Bless {
     });
   }
   ["renderTabList"]() {
-    let _0x16826a = '';
-    privateKey.forEach((_0x86fa1, _0x404676) => {
-      if (_0x404676 === this.currentTabIndex) {
-        _0x16826a += "{blue-fg}{bold} Account " + (_0x404676 + 0x1) + " {/bold}{/blue-fg} ";
+    let _0x517ac9 = '';
+    privateKey.forEach((_0x942de3, _0x2d46b7) => {
+      if (_0x2d46b7 === this.currentTabIndex) {
+        _0x517ac9 += "{blue-fg}{bold} Account " + (_0x2d46b7 + 0x1) + " {/bold}{/blue-fg} ";
       } else {
-        _0x16826a += " Account " + (_0x404676 + 0x1) + " ";
+        _0x517ac9 += " Account " + (_0x2d46b7 + 0x1) + " ";
       }
     });
-    this.tabList.setContent("{center}" + _0x16826a + "{/center}");
+    this.tabList.setContent("{center}" + _0x517ac9 + "{/center}");
     this.screen.render();
   }
-  ["switchTab"](_0x151cdf) {
+  ["switchTab"](_0x482931) {
     this.tabs[this.currentTabIndex].hide();
-    this.currentTabIndex = _0x151cdf;
+    this.currentTabIndex = _0x482931;
     this.tabs[this.currentTabIndex].show();
     this.renderTabList();
     this.screen.render();
   }
-  ["log"](_0xc8011d = '', _0x4bb686 = '', _0x538932 = new a6_0x2028b2(), _0x58a1ef) {
-    const _0x1fcfb1 = privateKey.find(_0x3d2acc => _0x3d2acc.pk == _0x4bb686);
-    const _0x477526 = privateKey.indexOf(_0x1fcfb1);
-    if (_0x58a1ef === undefined) {
-      a6_0x231e47.info("Account " + (_0x477526 + 0x1) + " - " + _0xc8011d);
-      _0x58a1ef = '-';
+  ["log"](_0x542cb3 = '', _0x2098e6 = '', _0x1fbc22 = new a6_0x52ee42(), _0x4f6d6f) {
+    const _0x150267 = privateKey.find(_0x4d961e => _0x4d961e.pk == _0x2098e6);
+    const _0x227844 = privateKey.indexOf(_0x150267);
+    if (_0x4f6d6f === undefined) {
+      a6_0x18d1b5.info("Account " + (_0x227844 + 0x1) + " - " + _0x542cb3);
+      _0x4f6d6f = '-';
     }
-    const _0x455da9 = _0x538932.address ?? '-';
-    const _0x5d45f0 = _0x538932.balance ?? '-';
-    const _0xe9784 = _0x538932.point ?? {};
-    const _0xc61f3a = _0xe9784.totalPoints ?? '-';
-    const _0x26f1b2 = _0xe9784.today ?? '-';
-    const _0xd67bed = "\n    Address      : " + _0x455da9 + "\n    Balance      : " + _0x5d45f0 + " " + RPC.SYMBOL + "\n    Points       : Total " + _0xc61f3a + " | Today " + _0x26f1b2 + "\n\n    Status       : " + _0xc8011d + "\n    Delay        : " + _0x58a1ef + "\n    ";
-    this.tabs[_0x477526].setContent(_0xd67bed);
+    const _0x4cba77 = _0x1fbc22.address ?? '-';
+    const _0x1f7500 = _0x1fbc22.balance ?? '-';
+    const _0x137144 = _0x1fbc22.point ?? {};
+    const _0x35394a = _0x137144.totalPoints ?? '-';
+    const _0x26b2bf = _0x137144.today ?? '-';
+    let _0xb4ca28;
+    if (Config.DISPLAYPOINT) {
+      _0xb4ca28 = "\n      Address      : " + _0x4cba77 + "\n      Balance      : " + _0x1f7500 + " " + RPC.SYMBOL + "\n      Points       : Total " + _0x35394a + " | Today " + _0x26b2bf + "\n  \n      Status       : " + _0x542cb3 + "\n      Delay        : " + _0x4f6d6f + "\n      ";
+    } else {
+      _0xb4ca28 = "\n      Address      : " + _0x4cba77 + "\n      Balance      : " + _0x1f7500 + " " + RPC.SYMBOL + "\n  \n      Status       : " + _0x542cb3 + "\n      Delay        : " + _0x4f6d6f + "\n      ";
+    }
+    this.tabs[_0x227844].setContent(_0xb4ca28);
     this.screen.render();
   }
-  ["info"](_0x5cb613 = '') {
-    const _0x40eca6 = "\n{center}Info: " + _0x5cb613 + "{/center}\n";
-    this.infoBox.setContent(_0x40eca6);
+  ['info'](_0x38c031 = '') {
+    const _0x12f5ca = "\n{center}Info: " + _0x38c031 + "{/center}\n";
+    this.infoBox.setContent(_0x12f5ca);
     this.screen.render();
   }
   ["clearInfo"]() {
