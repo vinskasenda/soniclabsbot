c9bfeead0682326017f1927fb323b47d27e580c5
Vikitoshi <31087501+vinskasenda@users.noreply.github.com>
2024-10-22 19:12:51 +0700

    Update soniclabs.js

--- app/src/core/dapps/soniclabs.js
+++ app/src/core/dapps/soniclabs.js
@@ -1,5 +1,5 @@
 export class SONICLABS {
-  static ["URL"] = "https://airdrop.soniclabs.com/";
+  static ["URL"] = 'https://airdrop.soniclabs.com/';
   static ["ARCADEGAMECONTRACT"] = "0xA7C492D9B3BD057845e3bC080c250ad868518478";
   static ["KEYREGISTCONTRACT"] = "0x4Cc7b0ddCD0597496E57C5325cf4c73dBA30cdc9";
 }
