168c72c4ededb7ba5d7bd52f7220aefbc590ea63
Vikitoshi <31087501+vinskasenda@users.noreply.github.com>
2024-10-22 19:12:30 +0700

    Update rpc.js

--- app/src/core/network/rpc.js
+++ app/src/core/network/rpc.js
@@ -1,6 +1,6 @@
 export class RPC {
   static ["CHAINID"] = 0xfaa5;
-  static ["RPCURL"] = "https://rpc.testnet.soniclabs.com";
-  static ["EXPLORER"] = 'https://testnet.soniclabs.com/';
+  static ['RPCURL'] = "https://rpc.testnet.soniclabs.com";
+  static ["EXPLORER"] = "https://testnet.soniclabs.com/";
   static ["SYMBOL"] = 'S';
 }
