af4115c5d92481f63da1df0d2918f3f90eb12b5a
Vikitoshi <31087501+vinskasenda@users.noreply.github.com>
2024-10-22 22:20:37 +0700

    Update rpc.js

--- app/src/core/network/rpc.js
+++ app/src/core/network/rpc.js
@@ -1,58 +1,6 @@
-var a5_0x5b7c8c = function () {
-  var _0x55f9aa = true;
-  return function (_0xed94e3, _0x126e16) {
-    var _0x2be6e3 = _0x55f9aa ? function () {
-      if (_0x126e16) {
-        var _0x1a4c26 = _0x126e16.apply(_0xed94e3, arguments);
-        _0x126e16 = null;
-        return _0x1a4c26;
-      }
-    } : function () {};
-    _0x55f9aa = false;
-    return _0x2be6e3;
-  };
-}();
-var a5_0x40899d = a5_0x5b7c8c(this, function () {
-  return a5_0x40899d.toString().search('(((.+)+)+)+$').toString().constructor(a5_0x40899d).search("(((.+)+)+)+$");
-});
-a5_0x40899d();
-var a5_0x2921f7 = function () {
-  var _0x49d7ca = true;
-  return function (_0x3ea259, _0x546157) {
-    var _0x1e046e = _0x49d7ca ? function () {
-      if (_0x546157) {
-        var _0xe806b4 = _0x546157.apply(_0x3ea259, arguments);
-        _0x546157 = null;
-        return _0xe806b4;
-      }
-    } : function () {};
-    _0x49d7ca = false;
-    return _0x1e046e;
-  };
-}();
-var a5_0x88e41e = a5_0x2921f7(this, function () {
-  var _0x3fd2fd;
-  try {
-    var _0xb29103 = Function("return (function() {}.constructor(\"return this\")( ));");
-    _0x3fd2fd = _0xb29103();
-  } catch (_0x1e348c) {
-    _0x3fd2fd = window;
-  }
-  var _0x4c8312 = _0x3fd2fd.console = _0x3fd2fd.console || {};
-  var _0x3391ef = ["log", "warn", 'info', "error", "exception", "table", "trace"];
-  for (var _0x27f242 = 0x0; _0x27f242 < _0x3391ef.length; _0x27f242++) {
-    var _0x3e7371 = a5_0x2921f7.constructor.prototype.bind(a5_0x2921f7);
-    var _0x560883 = _0x3391ef[_0x27f242];
-    var _0x5876a7 = _0x4c8312[_0x560883] || _0x3e7371;
-    _0x3e7371.__proto__ = a5_0x2921f7.bind(a5_0x2921f7);
-    _0x3e7371.toString = _0x5876a7.toString.bind(_0x5876a7);
-    _0x4c8312[_0x560883] = _0x3e7371;
-  }
-});
-a5_0x88e41e();
 export class RPC {
-  static ["CHAINID"] = 0xfaa5;
-  static ["RPCURL"] = 'https://rpc.testnet.soniclabs.com';
-  static ['EXPLORER'] = 'https://testnet.soniclabs.com/';
-  static ['SYMBOL'] = 'S';
+  static ['CHAINID'] = 0xfaa5;
+  static ["RPCURL"] = "https://rpc.testnet.soniclabs.com";
+  static ["EXPLORER"] = "https://testnet.soniclabs.com/";
+  static ["SYMBOL"] = 'S';
 }
