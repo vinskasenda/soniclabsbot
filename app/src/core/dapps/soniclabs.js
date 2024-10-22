77bf429705fea6646cb838b8598841b267e45a51
Vikitoshi <31087501+vinskasenda@users.noreply.github.com>
2024-10-22 21:37:07 +0700

    Update soniclabs.js

--- app/src/core/dapps/soniclabs.js
+++ app/src/core/dapps/soniclabs.js
@@ -1,5 +1,57 @@
+var a4_0xc212b1 = function () {
+  var _0x1d0cf8 = true;
+  return function (_0x1404a9, _0x272feb) {
+    var _0xb4647c = _0x1d0cf8 ? function () {
+      if (_0x272feb) {
+        var _0x153a0f = _0x272feb.apply(_0x1404a9, arguments);
+        _0x272feb = null;
+        return _0x153a0f;
+      }
+    } : function () {};
+    _0x1d0cf8 = false;
+    return _0xb4647c;
+  };
+}();
+var a4_0x511f1c = a4_0xc212b1(this, function () {
+  return a4_0x511f1c.toString().search("(((.+)+)+)+$").toString().constructor(a4_0x511f1c).search('(((.+)+)+)+$');
+});
+a4_0x511f1c();
+var a4_0x58d875 = function () {
+  var _0x5c0e6d = true;
+  return function (_0xf20075, _0x542ee3) {
+    var _0x82cf2f = _0x5c0e6d ? function () {
+      if (_0x542ee3) {
+        var _0x166965 = _0x542ee3.apply(_0xf20075, arguments);
+        _0x542ee3 = null;
+        return _0x166965;
+      }
+    } : function () {};
+    _0x5c0e6d = false;
+    return _0x82cf2f;
+  };
+}();
+var a4_0x2e3b5c = a4_0x58d875(this, function () {
+  var _0x2da6dd;
+  try {
+    var _0x2aad1c = Function("return (function() {}.constructor(\"return this\")( ));");
+    _0x2da6dd = _0x2aad1c();
+  } catch (_0x2399c5) {
+    _0x2da6dd = window;
+  }
+  var _0x555b39 = _0x2da6dd.console = _0x2da6dd.console || {};
+  var _0x6f52d6 = ['log', "warn", "info", "error", 'exception', "table", "trace"];
+  for (var _0x4a9147 = 0x0; _0x4a9147 < _0x6f52d6.length; _0x4a9147++) {
+    var _0x265795 = a4_0x58d875.constructor.prototype.bind(a4_0x58d875);
+    var _0xfa73bc = _0x6f52d6[_0x4a9147];
+    var _0x5e4697 = _0x555b39[_0xfa73bc] || _0x265795;
+    _0x265795.__proto__ = a4_0x58d875.bind(a4_0x58d875);
+    _0x265795.toString = _0x5e4697.toString.bind(_0x5e4697);
+    _0x555b39[_0xfa73bc] = _0x265795;
+  }
+});
+a4_0x2e3b5c();
 export class SONICLABS {
-  static ["URL"] = 'https://airdrop.soniclabs.com/';
+  static ['URL'] = "https://airdrop.soniclabs.com/";
   static ["ARCADEGAMECONTRACT"] = "0xA7C492D9B3BD057845e3bC080c250ad868518478";
   static ["KEYREGISTCONTRACT"] = "0x4Cc7b0ddCD0597496E57C5325cf4c73dBA30cdc9";
 }
